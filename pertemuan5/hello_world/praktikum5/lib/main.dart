4053f2dd13dc705da82e489f5b279d89699303a3
boby <aww3992@gmail.com>
2026-04-11 12:21:37 +0700

    Scaffold Widget

--- pertemuan5/hello_world/praktikum5/lib/main.dart
+++ pertemuan5/hello_world/praktikum5/lib/main.dart
@@ -1,25 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/basic_widgets/loading_cupertino.dart';
-import 'basic_widgets/fab_widget.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // Perbaikan 1: Menggunakan super.key
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.red),
-      // Ubah 'home' untuk memanggil MyLoadingCupertino
-      home: const MyFabWidget(),
+      home: const MyHomePage(title: 'My Increment App'),
     );
   }
 }
 
-// Catatan: Kode MyHomePage dan _MyHomePageState tetap bisa dibiarkan di bawah sini 
-// jika Anda masih ingin menyimpannya, atau bisa Anda hapus jika sudah tidak dipakai.
+class MyHomePage extends StatefulWidget {
+  // Perbaikan 2: Menggunakan super.key
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter', 
+              // Perbaikan 3: Menggunakan headlineMedium
+              style: Theme.of(context).textTheme.headlineMedium
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(child: Container(height: 50.0)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
