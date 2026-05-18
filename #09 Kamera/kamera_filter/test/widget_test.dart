84194d16582b86d9d42290a50d0a60fe962b19ef
boby <aww3992@gmail.com>
2026-05-19 02:22:46 +0700

    praktikum 9

---
+++ #09 Kamera/kamera_filter/test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    // Camera requires platform channels, skipping widget test for now.
+    expect(true, isTrue);
+  });
+}
